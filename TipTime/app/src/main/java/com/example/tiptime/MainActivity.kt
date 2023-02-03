092bd5ee8c3cfea5048bb65e41eca38c1a0cf9b1
victorialangoe <89309733+victorialangoe@users.noreply.github.com>
2023-02-03 07:50:45 +0100

    Finished up task 3, clean code next

---
+++ TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -0,0 +1,94 @@
+package com.example.tiptime
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tiptime.ui.theme.TipTimeTheme
+import java.text.NumberFormat
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TipTimeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    TipTimeScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TipTimeScreen(){
+    var amountInput by remember {mutableStateOf("")}
+    val amount = amountInput.toDoubleOrNull() ?: 0.0
+    val tip = CalculateTip(amount)
+    Column(modifier = Modifier.padding(32.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)) {
+        Text(
+            text = stringResource(id = R.string.calculateTip),
+            fontSize = 24.sp,
+            modifier = Modifier.align(Alignment.CenterHorizontally) )
+        Spacer(modifier = Modifier.height(16.dp))
+        EditNumberField(value = amountInput, onValueChange = {amountInput = it})
+        Spacer(modifier = Modifier.height(24.dp))
+        Text(text = stringResource(id = R.string.billAmount) + " " + CalculateTip(amount),
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            fontWeight = FontWeight.Bold
+        )
+
+
+
+    }
+
+}
+
+@Composable
+fun EditNumberField(value: String, onValueChange: (String) -> Unit  ){
+
+    TextField(
+        value = value,
+        onValueChange = onValueChange,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        singleLine = true,
+        label = {Text(
+                text = stringResource(id = R.string.billAmount),
+                modifier = Modifier.fillMaxWidth()
+        )}
+    )
+}
+
+private fun CalculateTip(amount: Double, tipPercent: Double = 15.0): String{
+    val tip = tipPercent / 100 * amount
+    return NumberFormat.getCurrencyInstance().format(tip)
+
+}
+
+@Preview
+@Composable
+fun DefaultPreview(){
+    TipTimeTheme {
+        TipTimeScreen()
+    }
+}
+
